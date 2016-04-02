b6786e3459985d6f5abc970def570ae6837d99a6
Basti <basti@basti-VitualBox>
2016-04-02 15:08:29 +0200

    changes

--- sgm-workstation/install.sh
+++ sgm-workstation/install.sh
@@ -2,25 +2,25 @@
 # Sys: Ubuntu 15.10 (von PXE)
 # tasksel auswahl: 'Ubuntu GNOME Desktop'
 
-# Basis:
+# basic console tools:
 sudo apt-get install vim git htop
 
+# basic gui tools:
+sudo apt-get install shutter keepassx
 
-# Thunderbird mit Enigmail
+
+# Thunderbird mit Enigmail und gnupg2
 sudo apt-get install thunderbird thunderbird-locale-de
 sudo apt-get install enigmail gnupg2
 
 # Atom Editor
-wget -O atom-amd64.deb https://atom.io/download/deb
-sudo dpkg -i atom-amd64.deb
+wget -O ~/Downloads/atom-amd64.deb https://atom.io/download/deb && \
+sudo dpkg -i ~/Downloads/atom-amd64.deb && \
+rm ~/Downloads/atom-amd64.deb
 
-# Screenshot-Tools
-sudo apt-get install shutter
-
-sudo apt-get install keepassx
 
 # Vagrant
-
+# TODO
 
 
 # Tools
