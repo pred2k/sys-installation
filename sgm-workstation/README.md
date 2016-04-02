502a08ad1cef811219b8e7a931f7b2cbf370864f
Basti <basti@basti-VitualBox>
2016-04-02 14:55:31 +0200

    neue Dateien

---
+++ sgm-workstation/README.md
@@ -0,0 +1,17 @@
+# Systemeinstellungen
+
+* dotfiles installieren
+
+## Atom-Editor Plugins
+
+* markdown-preview-plus
+
+## Thuderbird Addons
+* DKIM Verifier
+  https://addons.mozilla.org/de/thunderbird/addon/dkim-verifier/
+
+* Paranoia
+  https://addons.mozilla.org/de/thunderbird/addon/paranoia/
+
+* Markdown Here
+  https://addons.mozilla.org/de/thunderbird/addon/markdown-here/
---
+++ sgm-workstation/install.sh
@@ -0,0 +1,33 @@
+# SGM Workstation (1. April 2016)
+# Sys: Ubuntu 15.10 (von PXE)
+# tasksel auswahl: 'Ubuntu GNOME Desktop'
+
+# Basis:
+sudo apt-get install vim git htop
+
+
+# Thunderbird mit Enigmail
+sudo apt-get install thunderbird thunderbird-locale-de
+sudo apt-get install enigmail gnupg2
+
+# Atom Editor
+wget -O atom-amd64.deb https://atom.io/download/deb
+sudo dpkg -i atom-amd64.deb
+
+# Screenshot-Tools
+sudo apt-get install shutter
+
+sudo apt-get install keepassx
+
+# Vagrant
+
+
+
+# Tools
+sudo apt-get install ruby
+
+# Terminal-Emulator Guake
+sudo apt-get install guake
+
+# Remmina (Remote-Desktop RDP, VNC)
+sudo apt-get install remmina remmina-plugin-rdp
