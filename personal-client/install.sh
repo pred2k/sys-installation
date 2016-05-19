fb271d00b897ebbc7e9c7ee29322f37004bc88a0
Sebastian Forst <sebastian@work>
2016-05-19 14:27:28 +0200

    jitsi and verycrypt added

--- personal-client/install.sh
+++ personal-client/install.sh
@@ -13,7 +13,32 @@ sudo apt-get install spotify-client
 
 sudo pip install youtube-dl
 
-# Hexchat (Xenial supported)
-sudo apt-add-repository -y ppa:gwendal-lebihan-dev/hexchat-stable  && \
+# HexChat - https://hexchat.github.io/ (Ubuntu Xenial supported)
+sudo apt-add-repository -y ppa:gwendal-lebihan-dev/hexchat-stable && \
 sudo apt-get -qq update && \
 sudo apt-get install hexchat
+
+
+# Install Jitsi (Check https://jitsi.org/ or https://download.jitsi.org/jitsi/debian/ for latest Version first!)
+# Installing the .deb also adds Jitsi repository to sources list and gpg key
+JITSI_VERSION=2.8.5426-1
+JITSI_ARCHITECTURE=amd64
+wget https://download.jitsi.org/jitsi/debian/jitsi_${JITSI_VERSION}_${JITSI_ARCHITECTURE}.deb && sudo dpkg -i jitsi_${JITSI_VERSION}_${JITSI_ARCHITECTURE}.deb && sudo apt-get install -f
+
+
+# Install Veracrypt
+
+# Get Veracrypt Publickey, if you dont have it already
+wget https://www.idrix.fr/VeraCrypt/VeraCrypt_PGP_public_key.asc
+gpg --with-fingerprint VeraCrypt_PGP_public_key.asc
+# Check if Fingerprint is 993B 7D7E 8E41 3809 828F  0F29 EB55 9C7C 54DD D393
+gpg --import VeraCrypt_PGP_public_key.asc
+
+# Download Veracrypt from https://veracrypt.codeplex.com/wikipage?title=Downloads
+ # veracrypt-1.17-setup.tar.bz2(.sig):
+wget -O veracrypt.tar.bz2 'https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=veracrypt&DownloadId=1537179&FileTime=130999181365600000&Build=21031'
+wget -O veracrypt.tar.bz2.sig 'https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=veracrypt&DownloadId=1537182&FileTime=130999181380930000&Build=21031'
+gpg --verify veracrypt.tar.bz2.sig
+
+tar xf veracrypt.tar.bz2
+sudo bash veracrypt-1.17-setup-gui-x64
