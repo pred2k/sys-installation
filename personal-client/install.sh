53a74ed3d2cd4dae9a38548af7ffce1ef93de68c
Sebastian Forst <sebastian@work>
2016-05-27 12:56:41 +0200

    small makeup

--- personal-client/install.sh
+++ personal-client/install.sh
@@ -7,8 +7,8 @@ sudo apt-get install gstreamer1.0-libav
 
 # Spotify Client:
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 && \
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list  && \
-sudo apt-get update && \
+echo 'deb http://repository.spotify.com stable non-free' | sudo tee /etc/apt/sources.list.d/spotify.list && \
+sudo apt-get -qq update && \
 sudo apt-get install spotify-client
 
 sudo -H pip install --upgrade pip
