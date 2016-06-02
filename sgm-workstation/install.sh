a0ceef0ffc48ebdbba876df17ea0b46a98a31941
Sebastian Forst <sebastian@work>
2016-06-02 11:00:42 +0200

    try to compile keepassx in container, little things added

--- sgm-workstation/install.sh
+++ sgm-workstation/install.sh
@@ -8,7 +8,9 @@ sudo apt-get install puppet facter lsb-release
 
 # Tools
 sudo apt-get install ruby
-sudo gem install bundler
+sudo gem install bundler hiera-eyaml
+sudo -H pip install --upgrade pip
+sudo -H pip install --upgrade git-lint httpie
 
 # basic gui tools:
 sudo apt-get install gitg shutter meld
@@ -18,7 +20,7 @@ sudo apt-get install thunderbird thunderbird-locale-de
 sudo apt-get install enigmail gnupg2
 
 
-# Atom Editor (also use this lines for client updates!)
+# Atom Editor (also use this lines for updates!)
 wget -O ~/Downloads/atom-amd64.deb https://atom.io/download/deb && \
 sudo dpkg -i ~/Downloads/atom-amd64.deb && \
 rm ~/Downloads/atom-amd64.deb
@@ -26,30 +28,27 @@ rm ~/Downloads/atom-amd64.deb
 # Atom metrics disabling (no stats to Google Analytics please!)
 apm disable metrics
 
+# Lint Packages for Puppet (req. by Atom Package 'linter-puppet-lint')
+gem install puppet-lint
+
 # Atom Packages
 apm install markdown-preview-plus
+apm install editorconfig
+apm install atom-alignment
 apm install language-docker
 apm install linter-docker
 apm install language-puppet
+apm install language-logstash
+apm install language-tick
 apm install linter-js-yaml
-# Lint Packages for Puppet
-gem install puppet-lint
 apm install linter-puppet-lint
 apm install linter-puppet-parse
 # Candy
 apm install file-icons
 
 
-# HipChat
-sudo su
-echo "deb http://hipchat.iventurecapital.com/clients/linux/apt stable main" > \
-  /etc/apt/sources.list.d/atlassian-hipchat.list
-wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add -
-apt-get update
-apt-get install hipchat
-
 # HipChat 4 from official Repo
-sudo sh -c 'echo "deb https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client $(lsb_release -c -s) main" > /etc/apt/sources.list.d/atlassian-hipchat4.list'
+sudo sh -c 'echo "deb [arch=amd64] https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client $(lsb_release -c -s) main" > /etc/apt/sources.list.d/atlassian-hipchat4.list'
 wget -O - https://atlassian.artifactoryonline.com/atlassian/api/gpg/key/public | sudo apt-key add -
 sudo apt-get update && \
 sudo apt-get install hipchat4
@@ -63,14 +62,14 @@ vagrant plugin install vagrant-r10k puppet deep_merge
 # sudo apt-get install guake
 
 # Remmina (Remote-Desktop RDP, VNC)
-sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
-sudo apt-get update
+sudo apt-add-repository -y ppa:remmina-ppa-team/remmina-next && \
+sudo apt-get -qq update && \
 sudo apt-get install remmina remmina-plugin-rdp libfreerdp-plugins-standard
 
 
 # KeePassX v2
 KP_VERSION=2.0.2
-gpg --recv-keys '164C 7051 2F79 2947 6764  AB56 FE22 C6FD 8313 5D45'
+gpg --keyserver hkps.pool.sks-keyservers.net --recv-keys '164C 7051 2F79 2947 6764  AB56 FE22 C6FD 8313 5D45'
 sudo apt-get build-dep keepassx
 sudo apt-get install ubuntu-dev-tools cmake zlib1g-dev libgcrypt11-dev
 cd ~/Downloads/
@@ -79,14 +78,25 @@ wget https://www.keepassx.org/releases/${KP_VERSION}/keepassx-${KP_VERSION}.tar.
 gpg --verify keepassx-${KP_VERSION}.tar.gz
 tar xf keepassx-${KP_VERSION}.tar.gz
 cd keepassx-${KP_VERSION}/
-mkdir build && cd build && \
-cmake .. && make
+mkdir build && cd build && cmake .. && make
 sudo make install
+
+# Compile KeePassX in Docker container
+KP_VERSION=2.0.2
+gpg --keyserver hkps.pool.sks-keyservers.net --recv-keys '164C 7051 2F79 2947 6764  AB56 FE22 C6FD 8313 5D45'
+mkdir -p keepassx-src
+cd keepassx-src
+wget https://www.keepassx.org/releases/${KP_VERSION}/keepassx-${KP_VERSION}.tar.gz
+wget https://www.keepassx.org/releases/${KP_VERSION}/keepassx-${KP_VERSION}.tar.gz.sig
+gpg --verify keepassx-${KP_VERSION}.tar.gz.sig || exit 1
+tar xf keepassx-${KP_VERSION}.tar.gz
+cd ..
+docker build -t mykeepassx/builder . && docker run --rm -v ${PWD}/keepassx-src/keepassx-${KP_VERSION}/:/src mykeepassx/builder mkdir /src/build && cd /src/build && cmake .. && make
 
 
 # Ansible
 sudo apt-add-repository -y ppa:ansible/ansible && \
-sudo apt-get update && \
+sudo apt-get -qq update && \
 sudo apt-get install ansible
 
 
@@ -99,3 +109,7 @@ git config --global color.ui "auto"
 
 # remove useless GNOME Apps:
 sudo apt-get purge cheese aisleriot gnome-mahjongg gnome-sudoku gnome-mines
+
+# Gnome Extensions:
+# 'Media control for Spotify'
+# https://extensions.gnome.org/extension/55/media-player-indicator/
